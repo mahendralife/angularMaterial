5ac88223f2d87e32aa6da90296a25cab465eae2e
Mahendra Singh <mahendra.life@hotmail.com>
2016-09-14 13:02:47 +0530

    Create search.php

---
+++ search.php
@@ -0,0 +1,24 @@
+[
+{
+	"id": "123",
+	"title": "Sports",
+	"views": "Views: 200",
+	"deals": "Trending",
+	"story": "Articals: 1000"
+},
+{
+	"id": "1",
+	"title": "Sports",
+		"views": "Views: 200",
+	"deals": "Trending",
+"story": "Articals: 1000"
+},
+{
+	"id": "3",
+	"title": "By default, md-autocomplete will cache results when performing a query. After the initial call is performed ,By default, md-autocomplete will cache results when performing a query. After the initial call is",
+	"views": "Views: 200",
+	"deals": "General",
+  "story": "Images: 10"
+}
+
+]
